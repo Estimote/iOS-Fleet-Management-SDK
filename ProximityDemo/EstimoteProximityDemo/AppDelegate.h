07b58565b935c859b01d7458cd4bd3b91509ad6c
greggS <greggscutum@gmail.com>
2013-09-12 12:20:50 +0200

    ProximitiDemo

---
+++ ProximityDemo/EstimoteProximityDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  EstimoteProximityDemo
+//
+//  Created by Grzegorz Krukiewicz-Gacek on 28.08.2013.
+//  Copyright (c) 2013 Estimote Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ProximityDemo/EstimoteProximityDemo/ProximityDemoViewController.h
@@ -0,0 +1,14 @@
+//
+//  ProximityDemoViewController.h
+//  Estimote Simulator
+//
+//  Created by Grzegorz Krukiewicz-Gacek on 24.07.2013.
+//  Copyright (c) 2013 Grzegorz Krukiewicz-Gacek. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "BeaconManager.h"
+
+@interface ProximityDemoViewController : UIViewController <BeaconManagerDelegate>
+
+@end
