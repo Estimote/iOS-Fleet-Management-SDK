7af0210808bd2c8f5a9aabab3c578b3799543e3b
Grzegorz Krukiewicz-Gacek <greggscutum@gmail.com>
2015-01-08 12:36:02 +0100

    Added Custom Trigger Example app. Added background modes to Triggers Example apps.

---
+++ Examples/nearables/CustomTriggerExample/CustomTriggerExample/ViewController.h
@@ -0,0 +1,17 @@
+//
+//  ViewController.h
+//  CustomTriggerExample
+//
+//  Created by Grzegorz Krukiewicz-Gacek on 07.01.2015.
+//  Copyright (c) 2015 Estimote Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+- (IBAction)hotOutsideSwitchValueChanged:(id)sender;
+- (IBAction)coldOutsideSwitchValueChanged:(id)sender;
+
+@end
+
