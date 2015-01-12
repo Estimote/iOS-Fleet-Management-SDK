b1e033e87a21d5a54af765543a56860c80fdd3b7
Marcin Klimek <marcin.klimek@estimote.com>
2015-01-12 17:02:25 +0100

    Nearables monitoring examples added

---
+++ Examples/nearables/MonitoringExample-Swift/MonitoringExample-Swift/MonitoringDetailsViewController.swift
@@ -0,0 +1,72 @@
+//
+//  MonitoringDetailsViewController.swift
+//  MonitoringExample-Swift
+//
+//  Created by Marcin Klimek on 09/01/15.
+//  Copyright (c) 2015 Estimote. All rights reserved.
+//
+
+import UIKit
+
+class MonitoringDetailsViewController: UIViewController, ESTNearableManagerDelegate
+{
+    var nearableManager:ESTNearableManager
+    var nearable:ESTNearable
+
+    @IBOutlet weak var enterSwitch: UISwitch!
+    @IBOutlet weak var exitSwitch: UISwitch!
+    
+    required init(coder aDecoder: NSCoder)
+    {
+        self.nearable = ESTNearable()
+        self.nearableManager = ESTNearableManager()
+        
+        super.init(coder: aDecoder)
+        
+        self.nearableManager.delegate = self;
+    }
+    
+    override func viewDidLoad()
+    {
+        /**
+        *  Setup title of the screen based on nearable type.
+        */
+        self.title = NSString(format: "Nearable: %@", nearable.nameForType(nearable.type))
+        
+        /**
+        *  Create Estimote Nearable Manager and start looking for
+        *  the nearable device selected on previous screen and keept
+        *  as self.nearable property.
+        */
+        self.nearableManager.startMonitoringForIdentifier(self.nearable.identifier)
+    }
+    
+    func nearableManager(manager: ESTNearableManager!, didEnterIdentifierRegion identifier: String!)
+    {
+        /**
+        *  Verify if Enter switch is on, show local notification if ON.
+        */
+        if (self.enterSwitch.on)
+        {
+            var notification:UILocalNotification = UILocalNotification();
+            notification.alertBody = "Enter region notification";
+            
+            UIApplication.sharedApplication().presentLocalNotificationNow(notification)
+        }
+    }
+    
+    func nearableManager(manager: ESTNearableManager!, didExitIdentifierRegion identifier: String!)
+    {
+        /**
+        *  Verify if Exit switch is on, show local notification if ON.
+        */
+        if (self.exitSwitch.on)
+        {
+            var notification:UILocalNotification = UILocalNotification();
+            notification.alertBody = "Exit region notification";
+            
+            UIApplication.sharedApplication().presentLocalNotificationNow(notification)
+        }
+    }
+
+}
---
+++ Examples/nearables/MonitoringExample-Swift/MonitoringExample-Swift/ViewController.swift
@@ -0,0 +1,136 @@
+//
+//  ViewController.swift
+//  MonitoringExample-Swift
+//
+//  Created by Marcin Klimek on 09/01/15.
+//  Copyright (c) 2015 Estimote. All rights reserved.
+//
+
+import UIKit
+
+class ESTTableViewCell: UITableViewCell
+{
+    
+    override init(style: UITableViewCellStyle, reuseIdentifier: String?)
+    {
+        
+        super.init(style: UITableViewCellStyle.Subtitle, reuseIdentifier: reuseIdentifier)
+    }
+    
+    required init(coder aDecoder: NSCoder)
+    {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+
+class ViewController: UIViewController,
+    UITableViewDelegate,
+    UITableViewDataSource,
+    ESTNearableManagerDelegate
+{
+    var nearables:Array<ESTNearable>!
+    var nearableManager:ESTNearableManager!
+    var tableView:UITableView!
+    
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        
+        self.title = "Pick Nearable to monitor for:";
+        
+        tableView = UITableView(frame: self.view.frame)
+        tableView.delegate = self
+        tableView.dataSource = self
+        self.view.addSubview(tableView)
+        
+        tableView.registerClass(ESTTableViewCell.classForCoder(), forCellReuseIdentifier: "CellIdentifier")
+        
+        nearables = []
+        nearableManager = ESTNearableManager()
+        nearableManager.delegate = self
+        nearableManager .startRangingForType(ESTNearableType.All)
+    }
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int
+    {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return nearables.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell
+    {
+        let cell = tableView.dequeueReusableCellWithIdentifier("CellIdentifier", forIndexPath: indexPath) as ESTTableViewCell
+        
+        let nearable = nearables[indexPath.row] as ESTNearable
+        var details:NSString = NSString(format:"Type: %@ RSSI: %zd", nearable.nameForType(nearable.type), nearable.rssi);
+        cell.textLabel?.text = NSString(format:"Identifier: %@", nearable.identifier);
+        cell.detailTextLabel?.text = details;
+        
+        var imageView = UIImageView(frame: CGRectMake(self.view.frame.size.width - 60, 30, 30, 30))
+        imageView.contentMode = UIViewContentMode.ScaleAspectFill
+        imageView.image = self.imageForNearableType(nearable.type)
+        cell.contentView.addSubview(imageView)
+        
+        return cell
+    }
+    
+    func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat
+    {
+        return 80
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath)
+    {
+        self.performSegueWithIdentifier("details", sender: indexPath)
+    }
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?)
+    {
+        if(segue.identifier == "details")
+        {
+            var monitVC:MonitoringDetailsViewController = segue.destinationViewController as MonitoringDetailsViewController
+            monitVC.nearable = self.nearables[(sender as NSIndexPath).row]
+        }
+    }
+    
+    // MARK: - ESTNearableManager delegate
+    
+    func nearableManager(manager: ESTNearableManager!, didRangeNearables nearables: [AnyObject]!, withType type: ESTNearableType)
+    {
+        self.nearables = nearables as Array<ESTNearable>
+        self.tableView.reloadData()
+    }
+    
+    func imageForNearableType(type: ESTNearableType) -> UIImage?
+    {
+        switch (type)
+        {
+        case ESTNearableType.Bag:
+            return  UIImage(named: "sticker_bag")
+        case ESTNearableType.Bike:
+            return UIImage(named: "sticker_bike")
+        case ESTNearableType.Car:
+            return UIImage(named: "sticker_car")
+        case ESTNearableType.Fridge:
+            return UIImage(named: "sticker_fridge")
+        case ESTNearableType.Bed:
+            return UIImage(named: "sticker_bed")
+        case ESTNearableType.Chair:
+            return UIImage(named: "sticker_chair")
+        case ESTNearableType.Shoe:
+            return UIImage(named: "sticker_shoe")
+        case ESTNearableType.Door:
+            return UIImage(named: "sticker_door")
+        case ESTNearableType.Dog:
+            return UIImage(named: "sticker_dog")
+        default:
+            return UIImage(named: "sticker_grey")
+        }
+    }
+}
+
