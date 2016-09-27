db3c66495383ed29c3c7e6fd679ee77858d0f66c
daverpl <dawidraczka@gmail.com>
2016-09-27 11:04:17 +0200

    Added Location Beacons bulk updater Swift and Objective-C app templates.

---
+++ Examples/objective-c/BulkUpdate/BulkUpdate/AppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.h
+//  LocationBulkUpdater
+//
+//  Created by Dawid Rączka on 07/09/16.
+//  Copyright © 2016 Estimote. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
+
---
+++ Examples/objective-c/BulkUpdate/BulkUpdate/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  BulkUpdate
+//
+//  Created by Dawid Rączka on 07/09/16.
+//  Copyright © 2016 Dawid Rączka. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
