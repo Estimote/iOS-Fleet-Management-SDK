e39111f3e7fc260ccfae0d7d3469be3f44082fc3
Błażej Chwiećko <blazej.chwiecko@me.com>
2017-06-30 16:26:02 +0200

    Swift ProximityContent example updated.

--- Examples/swift/ProximityContent/ProximityContent/Estimote/BeaconDetails.swift
+++ Examples/swift/ProximityContent/ProximityContent/Estimote/BeaconDetails.swift
@@ -5,21 +5,21 @@
 import UIKit
 
 class BeaconDetails {
-
+    
     let beaconName: String
     let backgroundColor: UIColor
-
+    
     init(beaconName: String, backgroundColor: UIColor) {
         self.beaconName = beaconName
         self.backgroundColor = backgroundColor
     }
-
+    
     convenience init(beaconName: String, beaconColor: ESTColor) {
         self.init(
             beaconName: beaconName,
             backgroundColor: BeaconDetails.backgroundColor(for: beaconColor))
     }
-
+    
     private static let backgroundColors: [ESTColor: UIColor] = [
         .icyMarshmallow: UIColor(red: 109/255.0, green: 170/255.0, blue: 199/255.0, alpha: 1.0),
         .blueberryPie:   UIColor(red:  36/255.0, green:  24/255.0, blue:  93/255.0, alpha: 1.0),
@@ -28,11 +28,11 @@ class BeaconDetails {
         .lemonTart:      UIColor(red: 195/255.0, green: 192/255.0, blue:  16/255.0, alpha: 1.0),
         .candyFloss:     UIColor(red: 219/255.0, green: 122/255.0, blue: 167/255.0, alpha: 1.0)
     ]
-
+    
     static let neutralColor = UIColor(red: 160/255.0, green: 169/255.0, blue: 172/255.0, alpha: 1.0)
-
+    
     private static func backgroundColor(for beaconColor: ESTColor) -> UIColor {
         return backgroundColors[beaconColor] ?? neutralColor
     }
-
+    
 }
--- Examples/swift/ProximityContent/ProximityContent/Estimote/CachingContentFactory.swift
+++ Examples/swift/ProximityContent/ProximityContent/Estimote/CachingContentFactory.swift
@@ -3,24 +3,24 @@
 //
 
 class CachingContentFactory: BeaconContentFactory {
-
+    
     private let beaconContentFactory: BeaconContentFactory
-
+    
     private var cachedContent = Dictionary<BeaconID, AnyObject>()
-
+    
     init(beaconContentFactory: BeaconContentFactory) {
         self.beaconContentFactory = beaconContentFactory
     }
-
-    func requestContent(for beaconID: BeaconID, completion: @escaping (_ content: AnyObject) -> ()) {
-        if let cachedContent: AnyObject = self.cachedContent[beaconID] {
+    
+    func requestContent(for beacon: CLBeacon, completion: @escaping (_ content: AnyObject) -> ()) {
+        if let cachedContent: AnyObject = self.cachedContent[beacon.beaconID] {
             completion(cachedContent)
         } else {
-            self.beaconContentFactory.requestContent(for: beaconID) { (content) in
-                self.cachedContent[beaconID] = content
+            self.beaconContentFactory.requestContent(for: beacon) { (content) in
+                self.cachedContent[beacon.beaconID] = content
                 completion(content)
             }
         }
     }
-
+    
 }
